987613432881402d6e380b92e1621bbe6c9bba43
Fran Gallardo <3gestudio@gmail.com>
2016-09-21 18:43:47 +0200

    Corregido error con la meta del document con lang

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
  * @package Theme
  */
 ?><!DOCTYPE html>
-<html lang="<?php language_attributes(); ?>">
+<html lang="es-ES" class="no-js">
 <head>
 	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
